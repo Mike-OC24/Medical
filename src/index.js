4b4a880070675b6223d5d00ccee2246cc44e2f9a
Mike-OC <rid-mc@mail.ru>
2022-12-28 20:31:03 +0400

    ***

--- src/index.js
+++ src/index.js
@@ -17,30 +17,115 @@ new Swiper('.swiper', {
    },
 });
 
+/*---вариант 1------*/
 
-let dropdownButton = document.querySelectorAll('.dropdown__button');
-let dropdownContent = document.querySelectorAll('.dropdown__content');
+let servicesDropdown = document.querySelectorAll('.services__dropdown');
+
+servicesDropdown.forEach((item) => {
 
 
+   let itemDropdownContent = item.querySelector('.dropdown__content')
 
-for (let i = 0; i <= dropdownButton.length; i++) {
-   dropdownButton[i].addEventListener('click',function() {
+   let itemHeight = itemDropdownContent.clientHeight;
+
+   itemDropdownContent.style.height = '0px';
+
+
+   item.querySelector('.dropdown__button').onclick = function() {
+
       this.classList.toggle('button-active');
 
-         if ([i]) {
-            dropdownContent.classList.toggle('shown');
-         };
-         
-      });
-   };
+      let dropdownContent =  this.closest('.services__dropdown').querySelector('.dropdown__content')
+
+      dropdownContent.classList.toggle('dropdown__content-shown')
+
+      if(dropdownContent.classList.contains('dropdown__content-shown')) {
+         itemDropdownContent.style.height = itemHeight + 'px';
+      } else 
+      {
+         itemDropdownContent.style.height = '0px';
+      }
+      
+   }
+});
 
 
 
-let row = document.querySelectorAll('.row');
+/*---Вариант 2---*/
+
+/*let dropdownHeadline = Array.from(document.querySelectorAll('.dropdown__headline'));
+
+for (let headline of dropdownHeadline) {
+   headline.addEventListener('click', () => {
+      headline.nextElementSibling.classList.toggle('hidden')
+   })
+}*/
+
+
+
+/*---Вариант 3-- РАБОТАЕТ*/ 
+
+/*let dropdownHeadline = document.querySelectorAll('.dropdown__headline');
+
+for (let i = 0; i < dropdownHeadline.length; i++) {
+   dropdownHeadline[i].addEventListener('click',function() {
+      
+      let dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "flex") {
+         dropdownContent.style.display = "none";
+      } else {
+         dropdownContent.style.display = "flex";
+      }
+
+      if (dropdownContent.style.maxHeight) {
+         dropdownContent.style.maxHeight = null;
+      } else {
+         dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px';
+      }
+
+   });
+};
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*let benefitsContentInner = document.querySelector('.benefits__content-inner');
+
+benefitsContentInner.forEach((item) => {
+
+   item.querySelector('.row--two').onclick = function() {
+
+      this.classList.add('row-click');
+
+      let info = this.closest('.benefits__content-inner').querySelector('.info--two')
+
+      info.classList.add('info-visible')
+   }
+})*/
+
+
+
+
+     
+   /*   if ( dropdownButton === 'button-active') {
+         dropdownContent.classList.toggle('shown');
+      };*/
+
+
+/*let row = document.querySelectorAll('.row');
 let visibleInfo = document.querySelectorAll('.info');
 
 for (let i = 0; i <= row.length; i++) {
    row[i].addEventListener('click',function() {
-      this.classList.toggle('row--one');
+      this.classList.toggle('row-click');
    });
-};
+};*/
